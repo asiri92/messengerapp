99fab25f5cfd4ba72b5e00a5a2fcccfb2b6926cf
Asiri Dissanayaka <asiri.dissanayaka92@gmail.com>
2017-10-01 21:11:06 +0530

    signin-form-and-finishing-touches

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input,Output,EventEmitter } from "@angular/core";
 import { Message } from "./message.model";
+import { MessageService } from "./message.service";
 
 @Component({
     selector: 'app-message',
@@ -25,8 +26,14 @@ export class MessageComponent {
 
     //color = 'red';
 
+    constructor(private messageService: MessageService){}
+
     onEdit(){
         //alert('it workerd');
         this.editClicked.emit('Voila,a new value');
+    }
+
+    onDelete(){
+            this.messageService.deleteMessage(this.message);
     }
 }
