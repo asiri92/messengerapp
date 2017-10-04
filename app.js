435d54613e207c716bc31eafb1223719bfd2585c
Asiri Dissanayaka <asirid@hbs.com>
2017-10-04 17:23:16 +0530

    sign-in & sign-up with JWT

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var mongoose = require('mongoose');
 
 var appRoutes = require('./routes/app');
 var messageRoutes = require('./routes/messages');
+var userRoutes = require('./routes/user');
 
 var app = express();
 mongoose.connect('127.0.0.1:27017/node-angular');
@@ -32,6 +33,7 @@ app.use(function (req, res, next) {
 });
 
 app.use('/message', messageRoutes); // order of this route is important. hence messageRoute comes first
+app.use('/user', userRoutes);
 app.use('/', appRoutes);
 
 
--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -14,6 +14,7 @@ import { LogoutComponent } from "./auth/logout.component";
 import { SignupComponent } from "./auth/signup.component";
 import { SigninComponent } from "./auth/signin.component";
 import { HttpModule } from '@angular/http';//unlocks the Angular2 Http Service
+import {AuthService} from "./auth/auth.service";
 
 @NgModule({
     declarations: [
@@ -34,7 +35,9 @@ import { HttpModule } from '@angular/http';//unlocks the Angular2 Http Service
         FormsModule, 
         routing, //added routing after exporting to set out custom routes
         ReactiveFormsModule,
-        HttpModule],
+        HttpModule
+    ],
+    providers: [AuthService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
